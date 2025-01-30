d0f94ab2fe371b48e5e08aba9d4e04602088e109
Rituraj Borpujari <mail.riturajborpujari@gmail.com>
2025-01-30 19:04:49 +0530

    initial commit
    - allows you to connect to xfer server to see a File Upload UI

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const multer = require("multer");
+const qrcode = require("qrcode");
+const express = require("express");
+
+const UPLOAD_DIR = "uploads/";
+const upload = multer({ dest: UPLOAD_DIR });
+const HTTP_PORT = 8080;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send(`
+    <form action="/files" method="post" enctype="multipart/form-data">
+      <input type="file" name="file"/>
+      <input type="submit" value="Upload file"/>
+    </form>
+  `);
+
+  res.end();
+});
+
+app.post("/files", upload.single("file"), (req, res) => {
+  const destpath = path.join(UPLOAD_DIR, req.file.originalname);
+  fs.copyFileSync(req.file.path, destpath);
+  fs.unlinkSync(req.file.path);
+
+  console.log("File received:", destpath);
+
+  res.status(201).end("File Accepted");
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log("File server started on", HTTP_PORT);
+  const interfaces = os.networkInterfaces();
+  delete interfaces.lo;
+
+  console.log("Please scan QR");
+  Object.entries(interfaces).forEach(([_, list]) => {
+    const entry = list.find((el) => el.family === "IPv4")
+
+    const url = `http://${entry.address}:${HTTP_PORT}`
+
+    qrcode.toString(url, { type: "terminal" }, (_, encoded_url) => {
+      console.log(encoded_url);
+    })
+  });
+});
