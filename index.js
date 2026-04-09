f3ffbc063096e3f774e88f3f84d387601e78de36
Rituraj Borpujari <mail.riturajborpujari@gmail.com>
2026-04-09 11:33:00 +0530

    minor non-breaking changes

--- index.js
+++ index.js
@@ -12,39 +12,40 @@ const HTTP_PORT = 8080;
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send(`
-    <form action="/files" method="post" enctype="multipart/form-data">
-      <input type="file" name="file"/>
-      <input type="submit" value="Upload file"/>
-    </form>
-  `);
+	res.send(`
+		<body style="font-size: 18pt; background-color: #181818; color: #efefef; display: flex; justify-content: center; align-items: center; height: 100vh;">
+			<form action="/files" method="post" enctype="multipart/form-data" style="box-shadow: 0 0 1px #666; padding: 16px;">
+			<input type="file" name="file"/>
+			<input type="submit" value="Upload file"/>
+			</form>
+		</body>
+	`);
 
-  res.end();
+	res.end();
 });
 
 app.post("/files", upload.single("file"), (req, res) => {
-  const destpath = path.join(UPLOAD_DIR, req.file.originalname);
-  fs.copyFileSync(req.file.path, destpath);
-  fs.unlinkSync(req.file.path);
+	const destpath = path.join(UPLOAD_DIR, req.file.originalname);
+	fs.copyFileSync(req.file.path, destpath);
+	fs.unlinkSync(req.file.path);
 
-  console.log("File received:", destpath);
+	console.log("File received:", destpath);
 
-  res.status(201).end("File Accepted");
+	res.status(201).end("File Accepted");
 });
 
 app.listen(HTTP_PORT, () => {
-  console.log("File server started on", HTTP_PORT);
-  const interfaces = os.networkInterfaces();
-  delete interfaces.lo;
+	console.log("INFO: File server started");
+	const interfaces = os.networkInterfaces();
+	delete interfaces.lo;
 
-  console.log("Please scan QR");
-  Object.entries(interfaces).forEach(([_, list]) => {
-    const entry = list.find((el) => el.family === "IPv4")
+	Object.entries(interfaces).forEach(([_, list]) => {
+		const entry = list.find((el) => el.family === "IPv4");
 
-    const url = `http://${entry.address}:${HTTP_PORT}`
-
-    qrcode.toString(url, { type: "terminal" }, (_, encoded_url) => {
-      console.log(encoded_url);
-    })
-  });
+		const url = `http://${entry.address}:${HTTP_PORT}`;
+		console.log(url);
+		qrcode.toString(url, { type: "terminal" }, (_, encoded_url) => {
+			console.log(encoded_url);
+		});
+	});
 });
